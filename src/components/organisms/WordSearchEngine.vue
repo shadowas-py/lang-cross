cd37931872d71f6992958165779531d9c3d4bdba
Wojciech Okularczyk <wojciech.okularczyk@coderscrew.pl>
2022-10-02 21:57:26 +0200

    showing word list filtered by regex pattern

--- src/components/organisms/WordSearchEngine.vue
+++ src/components/organisms/WordSearchEngine.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <button @click="clearLocalStorage">FIND</button>
+    <button @click="clearLocalStorage">CLEAR</button>
+    <button @click="getWordList">GET WORDS LIST</button>
+    <div id="word-list-container">
+      <ul v-if="wordList.length > 0" >
+        <li v-for="word in wordList" :key="word">{{word}}</li>
+      </ul>
+      <p v-else>NOTHING TO SHOW</p>
+    </div>
   </div>
 </template>
 
@@ -9,11 +16,12 @@ import fetchDictionary from '@/utils/fetch';
 import { ENG_DICTIONARY_URL } from '@/constants';
 import { ref, watch, watchEffect } from 'vue';
 
+const wordList = ref([]);
 // FOR DEBUG
 const isThereDict = ref(false);
 watch(isThereDict, () => {
   if (isThereDict.value) {
-    console.log(window.localStorage.getItem('engDict'));
+    // console.log(window.localStorage.getItem('engDict'));
   }
 });
 
@@ -21,7 +29,7 @@ function saveDictionary() {
   if (!window.localStorage.getItem('engDict')) {
     fetchDictionary(ENG_DICTIONARY_URL).then((res) => {
       if (res) {
-        window.localStorage.setItem('engDict', res);
+        window.localStorage.setItem('engDict', JSON.stringify(res));
         isThereDict.value = true;
       }
     });
@@ -32,10 +40,21 @@ watchEffect(() => {
   saveDictionary();
 });
 
+const PATTERN = /^ma...$/;
+function getWordList() {
+  const res = JSON.parse(window.localStorage.getItem('engDict')).filter((word) => PATTERN.test(word));
+  wordList.value = res;
+}
+
 // FOR DEBUG
 function clearLocalStorage() {
   window.localStorage.clear();
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+  #word-list-container *{
+    font-size: 1.5rem;
+    margin: 0 3rem;
+  }
+</style>
